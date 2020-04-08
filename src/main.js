938134e9f0c909c7d75d00d75b9ae5beb5a463a4
siyu <1036786614@qq.com>
2020-04-08 15:31:08 +0800

    modify profile

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import App from './App'
-// 如果引入的是 index.js，可以使用下面的简写，等价于 import router from './router/index.js'
 import router from './router'
 import './directives'
 import './components'
 import store from './store'
 import VueSweetalert2 from './plugins/vue-sweetalert2'
+import Message from './plugins/message'
 
 Vue.use(VueSweetalert2)
+Vue.use(Message)
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -4,18 +4,23 @@ import routes from './routes'
 
 Vue.use(Router)
 
-const router = new Router({
+const router =  new Router({
   mode: 'history',
+  linkExactActiveClass: 'active',
   routes
 })
 
-// 全局前置守卫
 router.beforeEach((to, from, next) => {
-  // 获取仓库里的登录信息
-  const auth = router.app.$options.store.state.auth
+  const app = router.app
+  const store = app.$options.store
+  const auth = store.state.auth
 
-  if (auth && to.path.indexOf('/auth/') !== -1) {
-    // 如果当前用户已登录，且目标路由包含 /auth/ ，就跳转到首页
+  app.$message.hide()
+
+  if (
+    (auth && to.path.indexOf('/auth/') !== -1) ||
+    (!auth && to.meta.auth)
+  ) {
     next('/')
   } else {
     next()
